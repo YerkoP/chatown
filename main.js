b383f0006f58bfe9a96396ab85f9eba8d2ca4a0f
Yerko Palma <yerko.palma.serrano@outlook.com>
2024-11-25 16:00:54 -0300

    init

---
+++ main.js
@@ -0,0 +1,52 @@
+import { showInfo } from './lib/messages'
+import { Chatroom } from './lib/chatroom'
+import { Peer } from 'peerjs'
+import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const keyButton = document.getElementById('key-btn')
+  const addButton = document.getElementById('add-btn')
+  const addPeerDialog = document.getElementById('add-peer-dialog')
+  const addPeerDialogCloseButton = document.querySelector('#add-peer-dialog button.close')
+  const addPeerButton = document.getElementById('add-peer-btn')
+  const newPeerKey = document.getElementById('new-peer-key')
+  const sendMessageButton = document.getElementById('send-msg-btn')
+  const messageInput = document.getElementById('msg-input')
+
+  keyButton.addEventListener('click', e => {
+    e.preventDefault()
+    if (window.peer && window.peer.id)
+    showInfo({ text: 'Your personal key is {cta}', cta: window.peer.id })
+  })
+
+  addButton.addEventListener('click', e => {
+    e.preventDefault()
+    addPeerDialog.showModal()
+  })
+
+  addPeerDialogCloseButton.addEventListener('click', e => {
+    e.preventDefault()
+    addPeerDialog.close()
+  })
+
+  addPeerButton.addEventListener('click', e => {
+    e.preventDefault()
+    if (window.peer) {
+      const peerId = newPeerKey.value
+      window.chat.addMember(peerId)
+      newPeerKey.value = ''
+      addPeerDialog.close()
+    }
+  })
+
+  sendMessageButton.addEventListener('click', e => {
+    e.preventDefault()
+    const msg = messageInput.value
+    window.chat.broadcast(msg)
+    messageInput.value = ''
+  })
+
+  const peer = new Peer((Math.random() + 1).toString(32).slice(2, 10))
+  window.chat = new Chatroom(peer)
+})
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  safelist: [
+    'text-sm', 'text-gray-400', 'text-center', 'underline', 'cursor-pointer', 'text-left', 'text-right',
+    'px-4', 'py-1', 'rounded-md', 'bg-slate-950', 'bg-slate-800', 'mb-2', 'rounded-lg'
+  ],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
