a8c5a3d492b29d6b882523c71346f6872229f861
Yerko Palma <yerko.palma.serrano@outlook.com>
2024-11-25 21:23:36 -0300

    feat: support mobile and sned with enter key

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import './style.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const keyButton = document.getElementById('key-btn')
+  const keyButtonSm = document.getElementById('key-btn-sm')
   const addButton = document.getElementById('add-btn')
+  const addButtonSm = document.getElementById('add-btn-sm')
   const addPeerDialog = document.getElementById('add-peer-dialog')
   const addPeerDialogCloseButton = document.querySelector('#add-peer-dialog button.close')
   const addPeerButton = document.getElementById('add-peer-btn')
@@ -17,6 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault()
     if (window.peer && window.peer.id)
     showInfo({ text: 'Your personal key is {cta}', cta: window.peer.id })
+  })
+
+  keyButtonSm.addEventListener('click', e => {
+    e.preventDefault()
+    if (window.peer && window.peer.id)
+    showInfo({ text: 'Your personal key is {cta}', cta: window.peer.id })
+  })
+
+  addButtonSm.addEventListener('click', e => {
+    e.preventDefault()
+    addPeerDialog.showModal()
   })
 
   addButton.addEventListener('click', e => {
@@ -44,6 +57,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const msg = messageInput.value
     window.chat.broadcast(msg)
     messageInput.value = ''
+  })
+
+  messageInput.addEventListener('keydown', e => {
+    if (e.altKey || e.ctrlKey) {
+      return;
+    }
+    if (e.key === 'Enter') {
+      const msg = messageInput.value
+      window.chat.broadcast(msg)
+      messageInput.value = ''
+    }
   })
 
   const peer = new Peer((Math.random() + 1).toString(32).slice(2, 10))
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   safelist: [
     'text-sm', 'text-gray-400', 'text-center', 'underline', 'cursor-pointer', 'text-left', 'text-right',
-    'px-4', 'py-1', 'rounded-md', 'bg-slate-950', 'bg-slate-800', 'mb-2', 'rounded-lg'
+    'px-4', 'py-1', 'rounded-md', 'bg-slate-950', 'bg-slate-800', 'mb-2', 'rounded-lg', 'block', 'inline-block'
   ],
   content: [
     "./index.html",
