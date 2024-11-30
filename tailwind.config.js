27a5e98f41040f3f75fc77bbb94648b2fbd4da39
Yerko Palma <yerko.palma.serrano@outlook.com>
2024-11-30 19:10:44 -0300

    feat: basic call stream

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   safelist: [
     'text-sm', 'text-gray-400', 'text-gray-200', 'text-center', 'underline', 'cursor-pointer', 'text-left', 'text-right',
-    'px-4', 'py-1', 'rounded-md', 'bg-slate-950', 'bg-slate-800', 'mb-2', 'rounded-lg', 'block', 'inline-block'
+    'px-4', 'py-1', 'rounded-md', 'bg-slate-950', 'bg-slate-800', 'mb-2', 'rounded-lg', 'block', 'inline-block',
+    'col-start-1', 'col-end-7', 'row-start-2', 'row-end-6', 'z-20'
   ],
   content: [
     "./index.html",
